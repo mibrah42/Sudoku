765e8be4d61c292682274eb6635d4e9d9bea3531
mibrah42 <mohamed.ibrahim1@uoit.net>
2018-03-11 14:11:54 -0400

    changed new board to start new puzzle, removed commented functions

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function solve(puzzle){
       solvedPuzzle = solution;
       populateSudoku(puzzle);
 
-    });``
+    });
 }
 
 startNewPuzzle()
@@ -89,24 +89,6 @@ function restartPuzzle(){
     }
   }
 }
-
-// function test(){
-// for(var i = 0; i < 9; i++){
-//     for(var j = 0; j < 9; j++){
-//       var cell = document.getElementById(i + "-" + j);
-//       var input = cell.getElementsByClassName("input-text")[0];
-//       if(solvedPuzzle["solution"][i][j] != 0 ){
-//         cell.style.color = "#000000";
-//         input.value = solvedPuzzle["solution"][i][j];
-//         input.readOnly = true;
-//       } else {
-//         input.value = "";
-//         input.readOnly = false;
-//         cell.style.color = "#74D3BB";
-//       }
-//     }
-//   }
-//   }
 
 if(!document.cookie.includes("firstTime")){
   // var audio = new Audio('popUp.mp3');
