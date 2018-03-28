d6fac7f0c85fe41ea314de0c5fcfaaa55818d43e
mibrah42 <mohamed.ibrahim1@uoit.net>
2018-03-28 15:01:07 -0400

    added solution

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var solvedPuzzle;
 
 function startNewPuzzle(){
+  document.getElementById('solutionTable').style.display = 'none';
+  document.getElementById('id03').style.display = 'none'
   $('#chooseDifficulty > input').each(function () {
     if (this.checked){
       console.log(this.value)
@@ -27,6 +29,7 @@ function solve(puzzle){
       // console.log(solution)
       solvedPuzzle = solution;
       populateSudoku(puzzle);
+      populateSolution(solution)
 
     });
 }
@@ -39,6 +42,7 @@ function populateSudoku(puzzle){
     for(var j = 0; j < 9; j++){
       var cell = document.getElementById(i + "-" + j);
       var input = cell.getElementsByClassName("input-text")[0];
+      input.style.backgroundColor = "white";
       if(puzzle["board"][i][j] != 0 ){
         cell.style.color = "#000000";
         input.value = puzzle["board"][i][j];
@@ -52,6 +56,24 @@ function populateSudoku(puzzle){
   }
 }
 
+function populateSolution(puzzle) {
+  // console.log(solvedPuzzle)
+  for (var i = 9; i < 18; i++) {
+    for (var j = 0; j < 9; j++) {
+      var cell = document.getElementById(i + "-" + j);
+      var input = cell.getElementsByClassName("input-text")[0];
+      if (puzzle["solution"][i-9][j] != 0) {
+        cell.style.color = "#000000";
+        input.value = puzzle["solution"][i-9][j];
+        input.readOnly = true;
+      } else {
+        input.value = "";
+        input.readOnly = false;
+        cell.style.color = "#74D3BB";
+      }
+    }
+  }
+}
 
 function checkSolution(){
   counter = 0
@@ -61,44 +83,61 @@ function checkSolution(){
       var input = cell.getElementsByClassName("input-text")[0];
       if (solvedPuzzle["solution"][i][j] != parseInt(input.value) ){
         counter += 1;
-        // if (input.value != ""){
-        // 	// input.style.backgroundColor = "#E26A6A";
-        // }
+        if (input.value != ""){
+          input.style.backgroundColor = "#E26A6A";
+        }
         // input.style.backgroundColor = "#F12E45";
       } 
         // input.style.backgroundColor = "white";
-      
+        input.readOnly = true;  
     }
   }
-
   if (counter > 0){
     alert("Your solution is incorrect!");
   } else {
     alert("Your solution is correct!");
   }
+  document.getElementById('solutionTable').style.display = 'block';
 }
 
-function restartPuzzle(){
-  for(var i = 0; i<9;i++){
-    for(var j = 0; j<9; j++){
-      var cell = document.getElementById(i + "-" + j);
-      var input = cell.getElementsByClassName("input-text")[0];
-      if(!input.readOnly){
-        input.value = "";
+function showChoice(){
+  document.getElementById('id02').style.display = 'block';
+}
+
+function showChoiceStartNew(){
+  document.getElementById('id03').style.display = 'block';
+}
+
+function restartPuzzle(choice){
+  if(choice == "yes"){
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        var cell = document.getElementById(i + "-" + j);
+        var input = cell.getElementsByClassName("input-text")[0];
+        if (!input.readOnly) {
+          input.value = "";
+        }
       }
     }
-  }
+    document.getElementById('id02').style.display = 'none'
+  } else {
+    document.getElementById('id02').style.display = 'none'
+  } 
+  // if (confirm("Are you sure you want to restart?")) {
+  // }
 }
 
-if(!document.cookie.includes("firstTime")){
-  // var audio = new Audio('popUp.mp3');
-  // audio.play();
-  document.cookie = "firstTime=false";
-  document.getElementById('id01').style.display='block';
-}
+// if(!document.cookie.includes("firstTime")){
+//   // var audio = new Audio('popUp.mp3');
+//   // audio.play();
+//   document.cookie = "firstTime=false";
+//   document.getElementById('id01').style.display='block';
+// }
 function showInstructions(){
   // var audio = new Audio('popUp.mp3');
   // audio.play();
   document.getElementById('id01').style.display='block';
 }
+
+
   
