06634d937fdbfe00f3c769d06708bcbeab14815d
mibrah42 <mohamed.ibrahim1@uoit.net>
2018-03-30 18:42:26 -0400

    created test file

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function startNewPuzzle(){
         url: "https://sugoku.herokuapp.com/board?difficulty=" + difficulty,
         dataType: 'json',
         success: function(puzzle){
-          // console.log(puzzle);
           solve(puzzle)  
         }
       });
@@ -33,15 +32,11 @@ function solve(puzzle){
 
   $.post('https://sugoku.herokuapp.com/solve', data)
     .done(function (solution) {
-      // console.log(solution)
       solvedPuzzle = solution;
       populateSudoku(puzzle);
       populateSolution(solution)
-
     });
 }
-
-
 
 function populateSudoku(puzzle){
   console.log(solvedPuzzle)
@@ -65,7 +60,6 @@ function populateSudoku(puzzle){
 }
 
 function populateSolution(puzzle) {
-  // console.log(solvedPuzzle)
   for (var i = 9; i < 18; i++) {
     for (var j = 0; j < 9; j++) {
       var cell = document.getElementById(i + "-" + j);
@@ -98,9 +92,7 @@ function checkSolution(){
         if (input.value != ""){
           input.style.backgroundColor = "#E26A6A";
         }
-        // input.style.backgroundColor = "#F12E45";
       } 
-        // input.style.backgroundColor = "white";
         input.readOnly = true;  
     }
   }
@@ -108,27 +100,17 @@ function checkSolution(){
     document.getElementById('feedback').style.display = 'block';
     document.getElementById('feedback').style.color = "#E26A6A";
     document.getElementById('feedback').innerHTML = 'Your solution is Incorrect';
-    // alert("Your solution is incorrect!");
   } else {
     document.getElementById('feedback').style.display = 'block';
     document.getElementById('confetti').style.display = 'block';
     document.getElementById('feedback').style.color = "#7FEBD0";
     document.getElementById('feedback').innerHTML = 'Your solution is Correct!';
-    // alert("Your solution is correct!");
   }
   document.getElementById('solutionTable').style.display = 'block';
 }
 
-function showChoice(){
-  document.getElementById('id02').style.display = 'block';
-}
-
-function showChoiceStartNew(){
-  document.getElementById('id03').style.display = 'block';
-}
-
-function showChoiceSubmit(){
-  document.getElementById('id04').style.display = 'block';
+function showChoice(id){
+  document.getElementById(id).style.display = 'block';
 }
 
 function restartPuzzle(choice){
@@ -146,20 +128,14 @@ function restartPuzzle(choice){
   } else {
     document.getElementById('id02').style.display = 'none'
   } 
-  // if (confirm("Are you sure you want to restart?")) {
-  // }
 }
 
 if(!document.cookie.includes("firstTime")){
-  // var audio = new Audio('popUp.mp3');
-  // audio.play();
   document.cookie = "firstTime=false";
   document.getElementById('id01').style.display='block';
 }
 
 function showInstructions(){
-  // var audio = new Audio('popUp.mp3');
-  // audio.play();
   document.getElementById('id01').style.display='block';
 }
 
--- tests.js
+++ tests.js
@@ -1,3 +1,23 @@
+// UNIT TESTS
+
+// Test startNewPuzzle() function
+
+// Test solve() function
+
+// Test populateSudoku() function
+
+// Test populateSolution() function
+
+// Test checkSolution() function
+
+// Test showChoice() function
+
+// Test restartPuzzle() function
+
+// Test showInstructions() function
+
+
+// Povides solution to puzzle
 function getSolved() {
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
